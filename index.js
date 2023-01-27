c578d0754449e879398002db5a57bbaf37ddaee5
MajidovDoniyor <majidov.doniyor007@gmail.com>
2023-01-27 11:10:44 +0500

    js

---
+++ index.js
@@ -0,0 +1,14 @@
+// const firstName = "108 gruh";
+// let age = 3;
+
+// console.log(firstName);
+// console.log(age);
+
+// let y = 2;
+// let x = 3;
+
+// let sum = ++x;
+
+// x = y + 2 + x;
+
+// console.log(x);
